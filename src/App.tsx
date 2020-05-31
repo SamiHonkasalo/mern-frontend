ecf462ef51df6283d793fdc93e67b93c64be8130
Sami Honkasalo <N3227@student.jamk.fi>
2020-05-31 18:13:53 +0300

    added router, changed eslint TS-plugins to older version due to some weird errors, created dummy page for users

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Users from './user/pages/Users';
 
 const App: React.FC = () => {
-    return <div className="App">Hello!</div>;
+    return (
+        <Router>
+            <Route path="/users" exact>
+                <Users />
+            </Route>
+        </Router>
+    );
 };
 
 export default App;
