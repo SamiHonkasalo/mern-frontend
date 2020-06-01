c6be7d9916761ab2b61d9d2f2843b4fd4ee11c78
Sami Honkasalo <N3227@student.jamk.fi>
2020-06-01 22:55:20 +0300

    map working

--- src/shared/components/UIElements/Map.tsx
+++ src/shared/components/UIElements/Map.tsx
@@ -11,8 +11,6 @@ interface Props {
 
 const Map: React.FC<Props> = (props: Props) => {
     const mapRef = useRef<HTMLDivElement>(null);
-    const test = process.env.REACT_APP_GOOGLE_API_KEY;
-    console.log(test);
 
     const { center, zoom } = props;
     useEffect(() => {
